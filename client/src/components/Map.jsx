7a43359dcf930ded29fa3caf116b158661b1f47e
EthanBagunas <epbagunas@gmail.com>
2024-08-10 19:23:31 +0800

    colored level buttons

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,5 +1,25 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Button } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const themeColors = [
+  "#00ff00", // Normal (Green)
+  "#ffff00", // Low (Yellow)
+  "#ffa500", // Medium (Red)
+  "#ff0000", // High (Purple)
+  "#800080"  // Extreme (Blue)
+];
+
+// Update the theme object with the new colors
+const themes = themeColors.map((color, index) => ({
+  borderRadius: 100,
+  backgroundColor: color,
+  padding: "10px 36px",
+  fontSize: "15px"
+}));
+
+
 
 const mapStyles = {
   width: '80%',
@@ -10,8 +30,6 @@ const mapStyles = {
 
 
 export class MapContainer extends Component {
-
-
   render() {
     return (
       <Map 
@@ -24,7 +42,14 @@ export class MapContainer extends Component {
             lng: 36.8233
           }
         }
-      />
+      >
+        <Button variant='contained' style={themes[0]}>Normal</Button>
+        <Button variant='contained' style={themes[1]}>Low</Button>
+        <Button variant='contained' style={themes[2]}>Medium</Button>
+        <Button variant='contained' style={themes[3]}>High</Button>
+        <Button variant='contained' style={themes[4]}>Extreme</Button>
+      </Map>
+
     );
   }
 }
