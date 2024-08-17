e72e2ec0541074fa6a12b97d0e2dc7ba08657515
EthanBagunas <epbagunas@gmail.com>
2024-08-17 17:44:39 +0800

    vite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Home from './components/Home';
+import Home from './components/Home.jsx';
 import Navbar from './components/Navbar.jsx';
 import RegistrationForm from './components/Signup.jsx'; // Remove the comment and import the SignupForm component
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import MapContainer from './components/Map/Map.jsx';
-import Test from './components/Test.jsx';
 function App() {
   return (
     <Router>
@@ -15,7 +14,6 @@ function App() {
                 <Route exact path="/" element={<Home />} /> 
                 <Route exact path="/signup" element={<RegistrationForm/>} /> 
                 <Route exact path ='/map' element ={<MapContainer />}/>
-                <Route exact path ='/test' element ={<Test />}/>
 
             </Routes>
             </div>
