cceffe4ee88e8df50d39aa2d1d56dc2659f66b6c
EthanBagunas <epbagunas@gmail.com>
2024-08-28 16:21:48 +0800

    send RegistrationOTP 1 design

--- client/src/components/Test.jsx
+++ client/src/components/Test.jsx
@@ -5,28 +5,34 @@ import Button from '@mui/material/Button';
 import { blue } from "@mui/material/colors";
 import OtpTimer from 'otp-timer';
 
+const resendOTP = () => {
+  console.log("OTP sent");
+}
+
+const validateOtp= () => {
+
+}
+
+
 const Test = () => {
-  const [showTimer, setShowTimer] = useState(false);
-  const handleClick = () => {
-    setShowTimer(true);
+  const [showTimer, setShowTimer] = useState(true);
+  const handleClick = (status) => {
+    setShowTimer(status);
     SendOTP();
   };
-
-  const SendOTP = () => {
-    console.log("OTP sent");
-  }
-
-  const paperStyle= {padding: 20, height:100, width: 200, margin: '20px auto',  display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '7%'}
+  
+  const paperStyle= {padding: 20, height:100, width: 300, margin: '20px auto',  display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '7%'}
   
     return (
         <Grid>
             <Paper elevation ={10} style={paperStyle}>
                 <div>
-                    <h5>Enter your Contact number</h5>
-                    <input type="text" placeholder="Enter the your Contact Number to send One-Time-Password" />
-                    <Button style={{textAlign: 'center'}}color="secondary"  onClick={handleClick}>Submit</Button>
+                    <h5>We've sent A One-Time-Password to the Contact Number you've given</h5>
+                    <input type="text" placeholder="Enter the OTP Here..." size= {35}/>
+                    <br />
+                    {showTimer && <OtpTimer seconds={10} resend= {() => handleClick(true)} text= "Resending OTP in ..." />}
+                    <Button style={{textAlign: 'center'}}color="secondary"  onClick={validateOtp}>Submit</Button>
                     <div style={{ color: '#ffffff', padding: '10px 20px', borderRadius: '5px', fontSize: '18px', fontWeight: 'bold', textAlign: 'center'}}>
-                    {showTimer && <OtpTimer seconds={10} />}
 
                     </div>
                 </div>
